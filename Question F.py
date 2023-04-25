02b29ea588b93404ddfb206849840ae5192f6a63
Shrestho Bidhu Saha <123165731+BidhuSaha@users.noreply.github.com>
2023-04-25 01:42:32 +0100

    Create Question F.py

---
+++ Question F.py
@@ -0,0 +1,11 @@
+user_question= input("\nQ:").capitalize()
+while True:
+            if user_question=='F':
+                print(f"Inflation as measured by the consumer price index reflects the annual percentage change in the cost to the average consumer of acquiring a basket of goods and services that may be fixed or changed at specified intervals, such as yearly. The Laspeyres formula is generally used."
+                        "\nU.K. inflation rate for 2022 was 3.78%, a 2.55% increase from 2022"
+                        "\nU.K. inflation rate for 2022 was 2.52%, a 2.30% increase from 2021"
+                        "\nU.K. inflation rate for 2021 was 2.32%, a 1.53% increase from 2020."
+                        "\nU.K. inflation rate for 2020 was 0.99%, a 0.75% decline from 2019."
+                        "\nU.K. inflation rate for 2019 was 1.74%, a 0.55% decline from 2018."
+                        "\nU.K. inflation rate for 2018 was 2.29%, a 0.27% decline from 2017.")
+                print(f"The most significant change in inflation  was on 2022")
